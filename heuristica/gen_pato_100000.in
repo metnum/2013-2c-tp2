c5aad9b30b13d913bab7e78c110a834aeb4e33bf
Andres Osinski <andres.osinski@gmail.com>
2013-10-06 23:35:42 -0300

    Casos patologicos texto

---
+++ heuristica/gen_pato_100000.in
@@ -0,0 +1,7 @@
+print 100
+print 3
+print 100
+for i in range(99):
+    print 10
+print 500
+print 1000
---
+++ heuristica/gen_pato_100_central.py
@@ -0,0 +1,10 @@
+print 100
+print 3
+print 100
+for i in range(43):
+    print 1
+print 600
+for i in range(55):
+    print 1
+print 500
+print 1000
